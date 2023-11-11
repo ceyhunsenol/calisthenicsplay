3406d9f67f12390c89a76288677627cee171d10f
Ceyhun Şenol <>
2023-11-12 01:28:58 +0300

    remove comment

--- calisthenics-root-api/main.go
+++ calisthenics-root-api/main.go
@@ -1,10 +1,5 @@
 package main
 
 func main() {
-	//cal := calisthenics.NewCalisthenicsContentService()
 	Run()
-	//cal.Refresh(calisthenics.RefreshRequest{
-	//	CacheType: "genre",
-	//	ID:        "213123213213123",
-	//})
 }
